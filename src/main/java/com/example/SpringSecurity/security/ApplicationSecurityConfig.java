8fee78ef706ed6063e22674e7bea310e8218eb70
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2021-08-24 14:47:24 +0430

    0.6.2 CSRF is OK

--- src/main/java/com/example/SpringSecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/SpringSecurity/security/ApplicationSecurityConfig.java
@@ -6,8 +6,8 @@ import static com.example.SpringSecurity.security.ApplicationUserRole.ADMIN;
 import static com.example.SpringSecurity.security.ApplicationUserRole.ADMIN_TRAINEE;
 import static com.example.SpringSecurity.security.ApplicationUserRole.STUDENT;
 
-import com.example.SpringSecurity.filters.CsrfLoggerFilter;
 import com.example.SpringSecurity.student.Student;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +24,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -40,15 +41,31 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-//        .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//        .and()
+        .csrf().disable()
         .authorizeRequests()
         .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
         .antMatchers("/api/**").hasRole(STUDENT.name())
         .anyRequest()
         .authenticated()
         .and()
-        .httpBasic();
+        .formLogin()
+        .loginPage("/login")
+        .permitAll().defaultSuccessUrl("/courses", true)
+        .passwordParameter("password")
+        .usernameParameter("username")
+        .and()
+        .rememberMe()
+        .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+        .key("fefrfgerferfef")
+        .rememberMeParameter("remember-me")
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+        .clearAuthentication(true)
+        .invalidateHttpSession(true)
+        .deleteCookies("JSESSIONID", "remember-me")
+        .logoutSuccessUrl("/login");
   }
 
   @Override
